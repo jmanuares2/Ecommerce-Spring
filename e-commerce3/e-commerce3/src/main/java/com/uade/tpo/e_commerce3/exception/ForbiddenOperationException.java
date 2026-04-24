ce8816f36b92995183ee2374d7aa57132349401c
Juan Manuel Ares <154918016+jmanuares2@users.noreply.github.com>
2026-04-24 00:30:52 -0300

    carpeta repetida

--- e-commerce3/e-commerce3/src/main/java/com/uade/tpo/e_commerce3/exception/ForbiddenOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.uade.tpo.e_commerce3.exception;
-
-public class ForbiddenOperationException extends RuntimeException {
-    public ForbiddenOperationException(String message) {
-        super(message);
-    }
-}
